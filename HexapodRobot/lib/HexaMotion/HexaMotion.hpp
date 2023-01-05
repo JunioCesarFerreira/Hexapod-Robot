05538a07206c2bc100ee57ecf48cf77693d584d1
Junio <jcf_ssp@hotmail.com>
2023-01-05 11:22:27 -0300

    Add parameter structures of gait movements.

--- HexapodRobot/lib/HexaMotion/HexaMotion.hpp
+++ HexapodRobot/lib/HexaMotion/HexaMotion.hpp
@@ -3,10 +3,83 @@
 
 #include <Leg.hpp>
 
+struct coxaMovimentParameters
+{
+	uint8_t forward;
+	uint8_t backward;
+};
+
+struct femurMovimentParameters
+{
+	uint8_t up;
+	uint8_t down;
+};
+
+struct tibiaMovimentParameters
+{
+	uint8_t close;
+	uint8_t open;
+};
+
+struct coxaParameters
+{
+	coxaMovimentParameters front;
+	coxaMovimentParameters middle;
+	coxaMovimentParameters back;	
+};
+
+struct motionParameters
+{
+	coxaParameters rightCoxa;
+	coxaParameters leftCoxa;
+	femurMovimentParameters femur;	
+	tibiaMovimentParameters tibia;
+};
+
+
 class HexaMotion
 {
     private:
         HexaLegs *hexa_legs;
+		motionParameters p =
+		{
+			.rightCoxa = {
+				.front = {
+					.forward = 35,
+					.backward = 25
+				},
+				.middle = {
+					.forward = 55,
+					.backward = 45
+				},
+				.back = {
+					.forward = 75,
+					.backward = 65
+				}
+			},
+			.leftCoxa = {
+				.front = {
+					.forward = 65,
+					.backward = 75
+				},
+				.middle = {
+					.forward = 45,
+					.backward = 55
+				},
+				.back = {
+					.forward = 30,
+					.backward = 40
+				}
+			},
+			.femur = {
+				.up = 25,
+				.down = 50
+			},
+			.tibia = {
+				.close = 5,
+				.open = 20
+			}
+		};
 
     public:
         HexaMotion(HexaLegs *hexaLegs)
@@ -63,70 +136,45 @@ class HexaMotion
             hexa_legs->waitMotion();
         }
 
+        // tripode cycle 
         inline void tripodGaitCycle(uint8_t vel=20)
         {
-            // ---------------------------------------------------
-            // parameters used in the movements    
-            // right coxa joints
-            const uint8_t right_front_coxa_forward = 35;
-            const uint8_t right_front_coxa_backward = 25;
-            
-            const uint8_t right_middle_coxa_forward = 55;
-            const uint8_t right_middle_coxa_backward = 45;
-
-            const uint8_t right_back_coxa_forward = 75;
-            const uint8_t right_back_coxa_backward = 65;
-
-            // left coxa joints
-            const uint8_t left_front_coxa_forward = 65;
-            const uint8_t left_front_coxa_backward = 75;
-            
-            const uint8_t left_middle_coxa_forward = 45;
-            const uint8_t left_middle_coxa_backward = 55;
-
-            const uint8_t left_back_coxa_forward = 30;
-            const uint8_t left_back_coxa_backward = 40;        
-            
-            // other joints
-            const uint8_t femur_up = 25;
-            const uint8_t femur_down = 50;
-
-            const uint8_t tibia_close = 5;
-            const uint8_t tibia_open = 20;
-            // ---------------------------------------------------
-
-            // tripode cycle 
-            hexa_legs->R1.movimentInTask(right_front_coxa_forward, femur_up,tibia_close, vel);
-            hexa_legs->R3.movimentInTask(right_back_coxa_forward, femur_up, tibia_close, vel);
-            hexa_legs->L2.movimentInTask(left_middle_coxa_forward, femur_up, tibia_close, vel);
-
+            // legs group1 up
+            hexa_legs->R1.movimentInTask(p.rightCoxa.front.forward, p.femur.up, p.tibia.close, vel);
+            hexa_legs->R3.movimentInTask(p.rightCoxa.back.forward, p.femur.up, p.tibia.close, vel);
+            hexa_legs->L2.movimentInTask(p.leftCoxa.middle.forward, p.femur.up, p.tibia.close, vel);
             hexa_legs->waitMotion();
 
-            hexa_legs->R1.movimentInTask(right_front_coxa_forward, femur_down, tibia_open, vel);
-            hexa_legs->R3.movimentInTask(right_back_coxa_forward, femur_down, tibia_open, vel);
-            hexa_legs->L2.movimentInTask(left_middle_coxa_forward, femur_down, tibia_open, vel);
-            
-            hexa_legs->L1.movimentInTask(left_front_coxa_forward, femur_up, tibia_close, vel);
-            hexa_legs->L3.movimentInTask(left_back_coxa_forward, femur_up, tibia_close, vel);
-            hexa_legs->R2.movimentInTask(right_middle_coxa_forward, femur_up, tibia_close, vel);
-
+            // legs group1 down
+            hexa_legs->R1.movimentInTask(p.rightCoxa.front.forward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->R3.movimentInTask(p.rightCoxa.back.forward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->L2.movimentInTask(p.leftCoxa.middle.forward, p.femur.down, p.tibia.open, vel);
             hexa_legs->waitMotion();
 
-            hexa_legs->R1.movimentInTask(right_front_coxa_backward, femur_down, tibia_open, vel);
-            hexa_legs->R3.movimentInTask(right_back_coxa_backward, femur_down, tibia_open, vel);
-            hexa_legs->L2.movimentInTask(left_middle_coxa_backward, femur_down, tibia_open, vel);
-            
-            hexa_legs->L1.movimentInTask(left_front_coxa_forward, femur_down, tibia_open, vel);
-            hexa_legs->L3.movimentInTask(left_back_coxa_forward, femur_down, tibia_open, vel);
-            hexa_legs->R2.movimentInTask(right_middle_coxa_forward, femur_down, tibia_open, vel);
-
+            // legs group2 up
+            hexa_legs->L1.movimentInTask(p.leftCoxa.front.forward, p.femur.up, p.tibia.close, vel);
+            hexa_legs->L3.movimentInTask(p.leftCoxa.back.forward, p.femur.up, p.tibia.close, vel);
+            hexa_legs->R2.movimentInTask(p.rightCoxa.middle.forward, p.femur.up, p.tibia.close, vel);
             hexa_legs->waitMotion();
-                        
-            hexa_legs->L1.movimentInTask(left_front_coxa_backward, femur_down, tibia_open, vel);
-            hexa_legs->L3.movimentInTask(left_back_coxa_backward, femur_down, tibia_open, vel);
-            hexa_legs->R2.movimentInTask(right_middle_coxa_backward, femur_down, tibia_open, vel);
+
+            // legs group1 backward
+            hexa_legs->R1.movimentInTask(p.rightCoxa.front.backward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->R3.movimentInTask(p.rightCoxa.back.backward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->L2.movimentInTask(p.leftCoxa.middle.backward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->waitMotion();
+
+            // legs group2 down
+            hexa_legs->L1.movimentInTask(p.leftCoxa.front.forward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->L3.movimentInTask(p.leftCoxa.back.forward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->R2.movimentInTask(p.rightCoxa.middle.forward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->waitMotion();
+
+            // legs group2 backward
+            hexa_legs->L1.movimentInTask(p.leftCoxa.front.backward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->L3.movimentInTask(p.leftCoxa.back.backward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->R2.movimentInTask(p.rightCoxa.middle.backward, p.femur.down, p.tibia.open, vel);
+            hexa_legs->waitMotion();
         }
-
 };
 
 #endif
