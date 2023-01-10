f1e3d451d5ee05987ee894a94c4fa18c27f84701
Junio <jcf_ssp@hotmail.com>
2023-01-10 00:00:44 -0300

    review defines MPU6050.

--- HexapodRobot/lib/MPU6050/MPU6050.cpp
+++ HexapodRobot/lib/MPU6050/MPU6050.cpp
@@ -2,7 +2,7 @@
 
 void MPU6050Class::write(uint8_t regAddress, uint8_t value)
 {
-	Wire.beginTransmission(MPU);  // Initialize communication
+	Wire.beginTransmission(MPU6050_ADDRESS);  // Initialize communication
 	Wire.write(regAddress);       // Record address to be written
 	Wire.write(value);            // Insert data value
 	Wire.endTransmission(true);   // Signals end of transmission
@@ -13,41 +13,41 @@ void MPU6050Class::begin(int sda, int scl)
 {
     // MPU6050 I2C protocol
     Wire.begin(sda, scl);
-    write(PWR_MGMT_1, 0x00);
-    //write(SMPLRT_DIV, 0x07);
-    write(CONFIG, 0x00);
+    write(MPU6050_PWR_MGMT_1, 0x00);
+    //write(MPU6050_SMPLRT_DIV, 0x07);
+    write(MPU6050_CONFIG, 0x00);
     //write(GYRO_CONFIG, 0x18);
-    write(ACCEL_CONFIG, 0x19);
+    write(MPU6050_ACCEL_CONFIG, 0x19);
 }
 
 mpuVector MPU6050Class::getAccel()
 {
     mpuVector reading;
-	Wire.beginTransmission(MPU);
-	Wire.write(ACCEL_XOUT_H);
+	Wire.beginTransmission(MPU6050_ADDRESS);
+	Wire.write(MPU6050_ACCEL_XOUT_H);
     Wire.endTransmission(false);         
-    Wire.requestFrom(MPU, 6, 1); // request 6 registers
-    reading.x.high = Wire.read(); // ACCEL_XOUT_H
-    reading.x.low = Wire.read(); // ACCEL_XOUT_L
-    reading.y.high = Wire.read(); // ACCEL_YOUT_H
-    reading.y.low = Wire.read(); // ACCEL_YOUT_L
-    reading.z.high = Wire.read(); // ACCEL_ZOUT_H
-    reading.z.low = Wire.read(); // ACCEL_ZOUT_L
+    Wire.requestFrom(MPU6050_ADDRESS, 6, 1); // request 6 registers
+    reading.x.high = Wire.read(); // MPU6050_ACCEL_XOUT_H
+    reading.x.low = Wire.read(); // MPU6050_ACCEL_XOUT_L
+    reading.y.high = Wire.read(); // MPU6050_ACCEL_YOUT_H
+    reading.y.low = Wire.read(); // MPU6050_ACCEL_YOUT_L
+    reading.z.high = Wire.read(); // MPU6050_ACCEL_ZOUT_H
+    reading.z.low = Wire.read(); // MPU6050_ACCEL_ZOUT_L
     return reading;
 }
 
 mpuVector MPU6050Class::getGyro()
 {
     mpuVector reading;
-	Wire.beginTransmission(MPU);
-	Wire.write(GYRO_XOUT_H);
+	Wire.beginTransmission(MPU6050_ADDRESS);
+	Wire.write(MPU6050_GYRO_XOUT_H);
     Wire.endTransmission(false);         
-    Wire.requestFrom(MPU, 6, 1); // request 6 registers
-    reading.x.high = Wire.read(); // ACCEL_XOUT_H
-    reading.x.low = Wire.read(); // ACCEL_XOUT_L
-    reading.y.high = Wire.read(); // ACCEL_YOUT_H
-    reading.y.low = Wire.read(); // ACCEL_YOUT_L
-    reading.z.high = Wire.read(); // ACCEL_ZOUT_H
-    reading.z.low = Wire.read(); // ACCEL_ZOUT_L
+    Wire.requestFrom(MPU6050_ADDRESS, 6, 1); // request 6 registers
+    reading.x.high = Wire.read(); // MPU6050_GYRO_XOUT_H
+    reading.x.low = Wire.read(); // MPU6050_GYRO_XOUT_L
+    reading.y.high = Wire.read(); // MPU6050_GYRO_YOUT_H
+    reading.y.low = Wire.read(); // MPU6050_GYRO_YOUT_L
+    reading.z.high = Wire.read(); // MPU6050_GYRO_ZOUT_H
+    reading.z.low = Wire.read(); // MPU6050_GYRO_ZOUT_L
     return reading;
 }
--- HexapodRobot/lib/MPU6050/MPU6050.h
+++ HexapodRobot/lib/MPU6050/MPU6050.h
@@ -3,29 +3,29 @@
 
 #include <Wire.h>
 
-#define 	   MPU   		      0x68
+#define MPU6050_ADDRESS         0x68
 // MPU6050 Registers
-#define        SMPLRT_DIV         0x19
-#define        CONFIG             0x1A
-#define        GYRO_CONFIG        0x1B
-#define        ACCEL_CONFIG       0x1C
-#define        ACCEL_XOUT_H       0x3B
-#define        ACCEL_XOUT_L       0x3C
-#define        ACCEL_YOUT_H       0x3D
-#define        ACCEL_YOUT_L       0x3E
-#define        ACCEL_ZOUT_H       0x3F
-#define        ACCEL_ZOUT_L       0x40
-#define        TEMP_OUT_H         0x41
-#define        TEMP_OUT_L         0x42
-#define        GYRO_XOUT_H        0x43
-#define        GYRO_XOUT_L        0x44
-#define        GYRO_YOUT_H        0x45
-#define        GYRO_YOUT_L        0x46
-#define        GYRO_ZOUT_H        0x47
-#define        GYRO_ZOUT_L        0x48
-#define        PWR_MGMT_1         0x6B
-#define        WHO_AM_I           0x75
-#define        CLIENT_ADDRESS     0xD0
+#define MPU6050_SMPLRT_DIV      0x19
+#define MPU6050_CONFIG          0x1A
+#define MPU6050_GYRO_CONFIG     0x1B
+#define MPU6050_ACCEL_CONFIG    0x1C
+#define MPU6050_ACCEL_XOUT_H    0x3B
+#define MPU6050_ACCEL_XOUT_L    0x3C
+#define MPU6050_ACCEL_YOUT_H    0x3D
+#define MPU6050_ACCEL_YOUT_L    0x3E
+#define MPU6050_ACCEL_ZOUT_H    0x3F
+#define MPU6050_ACCEL_ZOUT_L    0x40
+#define MPU6050_TEMP_OUT_H      0x41
+#define MPU6050_TEMP_OUT_L      0x42
+#define MPU6050_GYRO_XOUT_H     0x43
+#define MPU6050_GYRO_XOUT_L     0x44
+#define MPU6050_GYRO_YOUT_H     0x45
+#define MPU6050_GYRO_YOUT_L     0x46
+#define MPU6050_GYRO_ZOUT_H     0x47
+#define MPU6050_GYRO_ZOUT_L     0x48
+#define MPU6050_PWR_MGMT_1      0x6B
+#define MPU6050_WHO_AM_I        0x75
+#define MPU6050_CLIENT_ADDRESS  0xD0
 
 union mpuValue
 {
